6d9e1b7bb7715c289c83328f95d46a22e0d0c630
Ritika Dhakate <ritikadhakate152@gmail.com>
2025-08-26 18:15:21 +0530

    change in index.js again

--- WBackend/src/index.ts
+++ WBackend/src/index.ts
@@ -19,11 +19,11 @@ app.use(express.json());
 app.use('/api', weatherRoutes);
 
 // Serve frontend build
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'WFrontend/build')));
 
 // Catch-all for React Router
 app.get('/*', (_req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'WFrontend', 'build', 'index.html'));
 });
 
 
